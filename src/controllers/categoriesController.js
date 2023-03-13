1377cb8836fa79bfa5a63deb56c90bff45281e75
jaron morren <jaronmorren@gmail.com>
2023-03-12 22:25:57 -0300

    Fixed error in categoriesController

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -2,10 +2,10 @@ const { categoriesService } = require('../services');
 
 const createCategory = async (request, response) => {
   try {
-    const { name } = request.body;
-    if (!name) return response.status(400).json({ message: '"name" is required' });
+    const newCategory = request.body;
+    if (!newCategory.name) return response.status(400).json({ message: '"name" is required' });
 
-    const category = await categoriesService.createCategory(name);
+    const category = await categoriesService.createCategory(newCategory);
     return response.status(201).json(category);
   } catch (error) {
     return response.status(500).json({ message: error.message });
